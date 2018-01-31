36221a07ddd0167b1d92d1ed5a97f2afd5a64ab6
Julia Evans <julia@jvns.ca>
2018-01-31 16:30:52 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 extern crate libc;
 extern crate read_process_memory;
+extern crate elf;
+
+#[macro_use]
+extern crate failure;
+
 use read_process_memory::*;
 use libc::*;
+use failure::Error;
 
 mod proc_maps;
 use proc_maps::*;
@@ -11,7 +17,7 @@ fn main() {
     let pid: pid_t = args[1].parse().unwrap();
     let source = pid.try_into_process_handle().unwrap();
     let maps = get_proc_maps(pid).unwrap();
-    for map in maps {
+    for map in &maps {
         let path = map.pathname.as_ref();
         if path.is_some() && path.unwrap().contains("syscall") {
             continue;
@@ -23,6 +29,34 @@ fn main() {
             copy_map(&map, &source, PROT_READ | PROT_WRITE | PROT_EXEC);
         }
     }
+
+    let map = get_map(&maps, "bin/ruby", "r-xp").unwrap();
+    let file = open_elf_file(pid, &map).unwrap();
+    let addr = get_symbol_addr(&map, &file, "rb_mod_name").unwrap();
+    println!("{:?}", addr);
+}
+
+fn open_elf_file(pid: pid_t, map: &MapRange) -> Result<elf::File, Error> {
+    // Read binaries from `/proc/PID/root` because the target process might be in a different
+    // mount namespace. /proc/PID/root is the view of the filesystem that the target process
+    // has. (see the proc man page for more)
+    // So we read /usr/bin/ruby from /proc/PID/root/usr/bin/ruby
+    let map_path = map.pathname.as_ref().expect("map's pathname shouldn't be None");
+    let elf_path = format!("/proc/{}/root{}", pid, map_path);
+    elf::File::open_path(&elf_path)
+        .map_err(|_| format_err!("Couldn't open ELF file: {:?}", elf_path))
+}
+
+fn get_map(maps: &Vec<MapRange>, contains: &str, flags: &str) -> Option<MapRange> {
+    maps.iter()
+        .find(|ref m| {
+            if let Some(ref pathname) = m.pathname {
+                pathname.contains(contains) && &m.flags == flags
+            } else {
+                false
+            }
+        })
+    .map(|x| x.clone())
 }
 
 fn copy_map(map: &MapRange, source: &ProcessHandle, perms: i32) {
@@ -42,6 +76,42 @@ fn copy_map(map: &MapRange, source: &ProcessHandle, perms: i32) {
         println!("...success");
     }
 }
+
+fn get_symbol_addr(map: &MapRange, elf_file: &elf::File, symbol_name: &str) -> Option<usize> {
+        elf_symbol_value(elf_file, symbol_name).map(|addr| {
+            let load_header = elf_load_header(elf_file);
+            map.range_start + addr - load_header.vaddr as usize
+        })
+}
+
+fn elf_symbol_value(elf_file: &elf::File, symbol_name: &str) -> Option<usize> {
+    // TODO: maybe move this to goblin so that it works on OS X & BSD, not just linux
+    let sections = &elf_file.sections;
+    for s in sections {
+        for sym in elf_file
+            .get_symbols(&s)
+                .expect("Failed to get symbols from section")
+                {
+                    if sym.name == symbol_name {
+                        return Some(sym.value as usize);
+                    }
+                }
+    }
+    None
+}
+
+fn elf_load_header(elf_file: &elf::File) -> elf::types::ProgramHeader {
+    elf_file
+        .phdrs
+        .iter()
+        .find(|ref ph| {
+            ph.progtype == elf::types::PT_LOAD && (ph.flags.0 & elf::types::PF_X.0) != 0
+        })
+    .expect("No executable LOAD header found in ELF file. Please report this!")
+        .clone()
+}
+
+
 
 pub fn copy_address_raw<T>(
     addr: usize,
