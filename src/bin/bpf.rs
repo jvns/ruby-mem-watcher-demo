b2bcdd605034b78505a5d2cd85ce15b87604d515
Julia Evans <julia@jvns.ca>
2018-02-01 12:03:47 -0500

    wip

--- src/bin/bpf.rs
+++ src/bin/bpf.rs
@@ -6,7 +6,26 @@ use bcc_sys::bccapi::*;
 use std::ffi::CString;
 
 fn main() {
-    new_module("hi");
+    let code = "
+#include <uapi/linux/ptrace.h>
+struct readline_event_t {
+        u32 pid;
+        char str[80];
+} __attribute__((packed));
+BPF_PERF_OUTPUT(readline_events);
+int get_return_value(struct pt_regs *ctx) {
+        struct readline_event_t event = {};
+        u32 pid;
+        if (!PT_REGS_RC(ctx))
+                return 0;
+        pid = bpf_get_current_pid_tgid();
+        event.pid = pid;
+        bpf_probe_read(&event.str, sizeof(event.str), (void *)PT_REGS_RC(ctx));
+        readline_events.perf_submit(ctx, &event, sizeof(event));
+        return 0;
+}
+    ";
+    new_module(code);
 }
 
 fn new_module(code: &str) {
